d46b57ddd695fad582b7d9e529d50550f77cab48
lvkheen <lvkheen@gmail.com>
2020-06-19 16:14:43 +0300

    ecommerce v0.2 - angular injection

--- src/main/java/com/lvkheen/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/lvkheen/ecommerce/dao/ProductCategoryRepository.java
@@ -3,8 +3,10 @@ package com.lvkheen.ecommerce.dao;
 import com.lvkheen.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+@CrossOrigin("http://localhost:4200")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
 
